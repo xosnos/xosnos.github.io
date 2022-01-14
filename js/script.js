8d461a9447d0f2d33fecfcbe4e4a079082f23899
Steven Nguyen <steyen@umich.edu>
2022-01-13 19:05:46 -0500

    some bootstrap grid fixes and style changes

--- js/script.js
+++ js/script.js
@@ -1,8 +1,16 @@
-$(document).ready(function() {
+$(document).ready(() => {
   console.log("Hello Universe!");
+
+  // Navigation
+  $(".nav-link").on("click", function() {
+    $(".navbar-collapse").collapse('hide');
+    $(".nav-link").removeClass("active");
+    $(this).addClass("active");
+  });
+
+  $(".navbar-brand").on("click", () => {
+    $(".nav-link").removeClass("active");
+    $("#homeButton").addClass("active");
+  });
 });
 
-$(".nav-link").click(function() {
-  $(".nav-link").removeClass("active");
-  $(this).addClass("active");
-});
